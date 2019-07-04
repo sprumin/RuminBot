826f3aaff54dc066d6f112030761a54fb836eb87
sprumin <sprumin@gmail.com>
2019-07-04 18:09:56 +0900

    update record parser

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RuminBot
-Slackbot to Python
+## Slacker 를 이용하여 Python Slack Bot 제작
+Python 을 슬랙 봇을 제작하는 프로젝트입니다.
+
+### TODO
+- 리그 오브 레전드 op.gg 녹화 기능
+
--- run.py
+++ run.py
@@ -1,26 +1,67 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from slacker import Slacker
+from urllib import parse
 
-import asyncio
-import json
+import re
+import requests
 import settings
-import websocket
-import websockets
+import time
 
 
-# slack module setting
-slack = Slacker(settings.RUMIN_TOKEN)
+def send_message(message):
+    slack = Slacker(settings.RUMIN_TOKEN)
+    slack.chat.post_message(settings.ON_ON_BOT_TEST, message)
 
-# slack rtm setting
-response = slack.rtm.start()
-sock_endpoint = response.body["url"]
 
-async def execute_bot():
-    ws = await websockets.connect(sock_endpoint)
+def record_lol(url, driver):
+    driver.get(url)
+    driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5]}})")
+    driver.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['ko-KR', 'ko']}})")
 
+    driver.implicitly_wait(3)
+
+    try:
+        record_button = driver.find_elements_by_class_name("Recording")[0]
+    except:
+        print("Does not Game")
+    else:
+        record_button.click()
+
+
+def download_record(url, driver, games):
+    driver.get(url)
+    driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5]}})")
+    driver.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['ko-KR', 'ko']}})")
+    driver.implicitly_wait(3)
+
+    try:
+        download_button = driver.find_elements_by_id("right_match_replay")[0]
+    except:
+        print("Does not replay")
+    else:
+        download_button.click()
+        time.sleep(5)
+
+
+def main():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--safebrowsing-disable-download-protection")
+
+    driver = webdriver.Chrome("chromedriver", chrome_options=options)
+    users = ["아는척좀하지말장"]
+    games = list()
+
+    for user in users:
+        record_lol(f"https://www.op.gg/summoner/spectator/userName={parse.quote(user)}&", driver)
+        download_record(f"https://www.op.gg/summoner/userName={user}", driver, games)
+
+
+if __name__ == "__main__":
+    main()
+    """
     while True:
-        message_json = await ws.recv()
-        print(message_json)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(execute_bot())
-loop.run_forever()
+        main()
+        time.sleep(600)
+    """
